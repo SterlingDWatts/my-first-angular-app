d6ca18fdc41c96bbc675fa10dd69e033ab474bd8
Sterling <SterlingDWatts@gmail.com>
2020-07-30 23:24:01 -0700

    Setup Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { CoreModule } from './core/core.module';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CustomersModule } from './customers/customers.module';
 import { SharedModule } from './shared/shared.module';
+import { CustomersModule } from './customers/customers.module';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { OrdersModule } from './orders/orders.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CustomersModule, SharedModule, CoreModule ],
+  imports: [
+    BrowserModule,
+    CustomersModule,
+    OrdersModule,
+    AppRoutingModule,
+    SharedModule,
+    CoreModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
